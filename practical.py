02e3ef2bb67e0b551a40f42a013fcb9fdd2c7992
Yuvraj Kottalagi <112368037+SPIDER8458@users.noreply.github.com>
2024-05-03 10:13:05 +0530

    Update practical.py

--- practical.py
+++ practical.py
@@ -3,6 +3,6 @@ def sum(x,y):
   sum=x+y
   print("the sum is" , sum)
 
-x=int(input("enter the first no.")
-y=int(input("enter the second no. ")
+x=int(input("enter the first no."))
+y=int(input("enter the second no. "))
 sum(x,y)
